7af5e359cf335ad92bf044bd91faeefcee2e35ee
Louis Sebag <sebag.louis@gmail.com>
2023-02-27 12:28:58 +0100

    Model Product + Controller Product + View Product (index/new/create)

---
+++ db/migrate/20230227112056_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :category
+      t.float :price_per_day
+      t.integer :year_of_purchase
+      t.string :condition
+      t.references :user_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
