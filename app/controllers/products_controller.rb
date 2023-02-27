08a6710e750a16354e28c2940e1731e10bbe110a
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-02-27 16:37:43 +0100

    product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
 
   def index
     @products = Product.all
+  end
+
+  def show
   end
 
   def new
@@ -9,9 +13,37 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def show
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, status: :see_other
+  end
+
+  private
+
+  def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :category, :price_per_day, :year_of_purchase, :condition, :photos)
   end
 end
