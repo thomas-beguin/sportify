474fd19c396cffb841af2674eac1e557b088b4ea
thomas beguin <thom.beguin@gmail.com>
2023-02-27 16:36:10 +0100

    show of user in progress : current rent / previous rent / my products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources  :users, only: %i[show]
   root to: "products#index"
   resources :products do
     resources :bookings, only: %i[new create]
