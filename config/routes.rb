173fc68d444ba4b4e61bc7ce7341266af7775cf0
thomas beguin <thom.beguin@gmail.com>
2023-02-27 16:43:12 +0100

    links in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  resources  :users, only: %i[show]
+  resources  :users, only: %i[show edit]
   root to: "products#index"
   resources :products do
     resources :bookings, only: %i[new create]
