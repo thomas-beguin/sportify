846afd91deb832bebe89c9841837af0bda5da5c5
thomas beguin <thom.beguin@gmail.com>
2023-02-28 12:23:56 +0100

    admin default false && added addresses

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_100126) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
