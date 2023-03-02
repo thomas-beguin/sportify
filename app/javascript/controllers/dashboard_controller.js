401e06a849d23289328c19e26527eaea24d79b09
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-03-02 13:22:22 +0100

    dashboard

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -11,14 +11,18 @@ export default class extends Controller {
   client() {
     this.clientTarget.classList.remove("d-none")
     this.ownerTarget.classList.add("d-none")
-    this.btnClientTarget.classList.add("active")
-    this.btnOwnerTarget.classList.remove("active")
+    this.btnClientTarget.classList.remove("btn-outline-primary")
+    this.btnClientTarget.classList.add("active btn-primary text-white font-weight-bold")
+    this.btnOwnerTarget.classList.remove("active btn-primary text-white font-weight-bold")
+    this.btnOwnerTarget.classList.add("btn-outline-primary")
   }
 
   owner() {
     this.clientTarget.classList.add("d-none")
     this.ownerTarget.classList.remove("d-none")
-    this.btnClientTarget.classList.remove("active")
-    this.btnOwnerTarget.classList.add("active")
+    this.btnClientTarget.classList.remove("active btn-primary text-white font-weight-bold")
+    this.btnClientTarget.classList.add("btn-outline-primary")
+    this.btnOwnerTarget.classList.remove("btn-outline-primary")
+    this.btnOwnerTarget.classList.add("active btn-primary text-white font-weight-bold")
   }
 }
