a85bb2c34d5a276c117c621758c8289225511570
Louis Sebag <sebag.louis@gmail.com>
2023-03-02 12:01:22 +0100

    Stimulus + Map

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -16,4 +16,5 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
-
+import ShowmapController from "./showmap_controller"
+application.register("showmap", ShowmapController)
