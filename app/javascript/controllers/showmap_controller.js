6d0e30b1ad9f30bb413e31d876d90db601a29bc0
thomas beguin <thom.beguin@gmail.com>
2023-03-03 16:29:20 +0100

    done

--- app/javascript/controllers/showmap_controller.js
+++ app/javascript/controllers/showmap_controller.js
@@ -24,7 +24,7 @@ export default class extends Controller {
     navigator.geolocation.getCurrentPosition((data) => {
       const lat = data.coords.latitude;
       const lon = data.coords.longitude;
-      const url = `http://localhost:3000/?current_position%5Blat%5D=${lat}&current_position%5Blon%5D=${lon}&commit=current_location`;
+      const url = `https://sportify-thomas-beguin.osc-fr1.scalingo.io/?current_position%5Blat%5D=${lat}&current_position%5Blon%5D=${lon}&commit=current_location`;
       window.location.href = url ;
     });
 
